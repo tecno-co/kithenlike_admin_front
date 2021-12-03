2255fdc3f38bacd80c1c4d7ac0becc77c166f6ee
Kevin Garzón <kevinegp13@gmail.com>
2021-12-03 10:40:08 -0500

    Se corrigen conflictos

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  // API: 'https://prodkitchenlikeapi.tk' // Producción
-  API: 'https://prodkitchenlikeapi.tk/develop' // Pruebas
+  API: 'https://prodkitchenlikeapi.tk' // Producción
+  // API: 'https://prodkitchenlikeapi.tk/develop' // Pruebas
 };
